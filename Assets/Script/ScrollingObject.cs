220d71a113d58ea7bc9ca573dad5c001a5ddfcae
persona2740 <80808162+persona2740@users.noreply.github.com>
2021-04-14 14:35:21 +0900

    UI만듬

--- Assets/Script/ScrollingObject.cs
+++ Assets/Script/ScrollingObject.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class ScrollingObject : MonoBehaviour
 {
     public float speed = 10f;
+
     private void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (!GameManager.instance.isGameover)
+  
+         { 
+        transform.Translate(Vector3.left* speed * Time.deltaTime);
+         }
     }
 }
 
